c3093c2d18f835ec3e534e56218a4864dc47f1bc
Chetanrathore-13 <chetanrathore9131@gmail.com>
2025-04-03 15:21:41 +0530

    some files updated

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    refreshtoken: {
+        type: String,
+    },
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
