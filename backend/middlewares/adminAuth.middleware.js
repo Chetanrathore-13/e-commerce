4b31d9c3fb29dd9866324bb2723e93ac705f74ca
Chetanrathore-13 <chetanrathore9131@gmail.com>
2025-04-12 14:45:34 +0530

    controller directory set up

--- backend/middlewares/adminAuth.middleware.js
+++ backend/middlewares/adminAuth.middleware.js
@@ -1,21 +1,3 @@
-// import jwt from 'jsonwebtoken'
-
-// const adminAuthMiddleware = async (req, res, next) => {
-//     try {
-//         const token = req.cookies.accessToken;
-//         if (!token) {
-//             return res.status(401).json({ message: "Access Denied. No token provided." });
-//         }
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         req.adminId = decoded.id;
-//         next();
-//     } catch (error) {
-//         res.status(500).json({ message: error.message });
-//     }
-// };
-
-// export default adminAuthMiddleware
-
 import jwt from "jsonwebtoken";
 
 // You can store these secrets in your environment or configuration file
