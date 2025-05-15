eef47cf89625760d8d435cf222b591cb21ba78e3
Chetanrathore-13 <chetanrathore9131@gmail.com>
2025-05-15 13:03:42 +0530

    header hydration error fixed

--- components/header.tsx
+++ components/header.tsx
@@ -356,20 +356,7 @@ export default function Header() {
                               } hover:text-teal-800 border-b`}
                             >
                               {category.name}
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="h-4 w-4"
-                              >
-                                <path d="m6 9 6 6 6-6" />
-                              </svg>
+                              
                             </Link>
                             {category.subcategories && category.subcategories.length > 0 && (
                               <div className="pl-4">
